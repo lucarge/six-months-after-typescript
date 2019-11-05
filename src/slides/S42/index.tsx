0b7ccfd4576099e809dc6dfc44e580a40f1308da
lucarge <luca.argenziano@icloud.com>
2019-11-05 14:35:06 +0100

    improvement: improve the animation of slide 42

--- src/slides/S42/index.tsx
+++ src/slides/S42/index.tsx
@@ -2,19 +2,43 @@ import { Column } from "components/grid/column";
 import { Row } from "components/grid/row";
 import { FlowIcon } from "components/icons/flow";
 import { TypeScriptIcon } from "components/icons/typescript";
-import React from "react";
-import { Appear } from "spectacle";
+import React, { useState } from "react";
+import { Anim, Appear } from "spectacle";
 
-export const S42 = () => (
-  <Row backgroundColor="#2d343a">
-    <Column alignItems="center" flexBasis="0" flexGrow={1} justifyContent="center">
-      <FlowIcon height="20vw" width="20vw" />
-    </Column>
+export const S42 = () => {
+  const [grow, setGrow] = useState(1);
 
-    <Appear>
-      <Column alignItems="center" backgroundColor="#007ACC" flexBasis="0" flexGrow={1} justifyContent="center">
-        <TypeScriptIcon height="20vw" width="20vw" />
+  const handleAnim = (forwards?: boolean) => {
+    if (forwards) {
+      setGrow(0.0001);
+
+      return;
+    }
+
+    setGrow(1);
+  };
+
+  return (
+    <Row backgroundColor="#2d343a">
+      <Column alignItems="center" flexBasis="0" flexGrow={grow} justifyContent="center" transition="all 1s">
+        <FlowIcon height="20vw" width="20vw" />
       </Column>
-    </Appear>
-  </Row>
-);
+
+      <Appear>
+        <Column alignItems="center" backgroundColor="#007ACC" flexBasis="0" flexGrow={1} justifyContent="center">
+          <TypeScriptIcon height="20vw" width="20vw" />
+        </Column>
+      </Appear>
+
+      <Anim
+        easing="back"
+        fromStyle={{ display: "none " }}
+        onAnim={handleAnim}
+        toStyle={[{ display: "none" }]}
+        transitionDuration={0}
+      >
+        <Column display="none" />
+      </Anim>
+    </Row>
+  );
+};
