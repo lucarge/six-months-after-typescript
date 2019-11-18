4f5a31724a1245a73230c5be72c660f9265a12bf
lucarge <luca.argenziano@icloud.com>
2019-11-18 16:50:13 +0100

    feat(#5): improvements to "why TypeScript" slides

--- src/slides/S41/index.tsx
+++ src/slides/S41/index.tsx
@@ -2,18 +2,26 @@ import { Column } from "components/grid/column";
 import { Row } from "components/grid/row";
 import { GithubIcon } from "components/icons/github";
 import React from "react";
-import { Appear, Text } from "spectacle";
+import { Appear, Heading, Text } from "spectacle";
 
 export const S41 = () => (
   <>
-    <Column paddingBottom="2.66rem">
-      <Row alignItems="center" justifyContent="flex-start">
-        <GithubIcon height="2.66rem" width="2.66rem" />
-        <Text margin="0.25rem 0.5rem" textFont="tertiary">
-          lucarge/<b>ts-immutable-playgroud</b>
-        </Text>
-      </Row>
+    <Column paddingBottom={96}>
+      <Heading margin="0" size={4} textAlign="left" textColor="#222">
+        Proofs of concept
+      </Heading>
     </Column>
+
+    <Appear>
+      <Column paddingBottom="2.66rem">
+        <Row alignItems="center" justifyContent="flex-start">
+          <GithubIcon height="2.66rem" width="2.66rem" />
+          <Text margin="0.25rem 0.5rem" textFont="tertiary">
+            lucarge/<b>ts-immutable-playgroud</b>
+          </Text>
+        </Row>
+      </Column>
+    </Appear>
 
     <Appear>
       <Column paddingBottom="2.66rem">
--- src/slides/S43/index.tsx
+++ src/slides/S43/index.tsx
@@ -5,7 +5,7 @@ import { Appear, Heading } from "spectacle";
 export const S43 = () => (
   <Appear>
     <Column backgroundColor="#007ACC" borderRadius="100%" flexGrow={1} padding="10rem">
-      <Heading size={2}>TypeScript all the things!!</Heading>
+      <Heading size={2}>TypeScript all things!!</Heading>
     </Column>
   </Appear>
 );
