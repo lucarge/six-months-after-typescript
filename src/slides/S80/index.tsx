f825b7d1d8eb584b474322edd49dfd3b1d9ea3cd
lucarge <luca.argenziano@icloud.com>
2019-11-19 18:27:13 +0100

    feat(#9): update the slide about conclusions

--- src/slides/S80/index.tsx
+++ src/slides/S80/index.tsx
@@ -1,4 +1,29 @@
+import { Column } from "components/grid/column";
 import React from "react";
-import { Heading } from "spectacle";
+import { Appear, Heading, Text } from "spectacle";
 
-export const S80 = () => <Heading>Conclusions</Heading>;
+export const S80 = () => (
+  <>
+    <Column paddingBottom={48}>
+      <Heading>Conclusions</Heading>
+    </Column>
+
+    <Appear>
+      <Column paddingBottom={48}>
+        <Text textColor="secondary">TS has definitely brought value in ProntoPro.</Text>
+      </Column>
+    </Appear>
+
+    <Appear>
+      <Column paddingBottom={48}>
+        <Text textColor="secondary">It's been a bumpy ride so far, but we're having fun!</Text>
+      </Column>
+    </Appear>
+
+    <Appear>
+      <Column paddingBottom={48}>
+        <Text textColor="secondary">TS is not a must-have: context is the king.</Text>
+      </Column>
+    </Appear>
+  </>
+);
